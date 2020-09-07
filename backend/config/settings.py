1ac8133ee1b787a669689cadbfac4ecb7a7c04da
mateusolorenzatti <mateusolorenzatti@gmail.com>
2020-09-06 22:55:00 -0300

    Efetuado o Deploy no Heroku

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,8 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = ['http://localhost:4200',]
+CORS_ORIGIN_WHITELIST = ['https://kefnum.herokuapp.com',
+                 'https://localhost', 'https://127.0.0.1', 'https://0.0.0.0',]
 
 # Application definition
 
